f768c6f08dc9abc058e02cb81e4587659e3e1d9c
Bogdan Gava <63742326+BogdanGava@users.noreply.github.com>
2021-01-13 21:30:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,9 @@ int main()
     item i1("Claymore", 35.6, 8);
     item i2("Saw",35, 12 );
     npc n1("Ex", 18, 1);
-    npc n2("Ex2", 20,0);
+    npc n2("Ex2", 15,0);
+    n1.adauga(i1);
+    n1.adauga(i2);
     n1.interactioneaza(c1, i1);
     n2.interactioneaza(c1, i1);
 //    i1.afisare();
@@ -23,8 +25,11 @@ int main()
 //    c2.adauga(i1);
 //    cout<<i1;
 //    cout<<i2;
-    cout<<n1;
+//    cout<<n1;
     cout<<c1;
 //    cout<<c2;
+    /* caracterul 1 are level 19 si nu are niciun item in inventar.
+     Acesta va interactiona cu npc 1 ce are 2 iteme, din moment ce npc-ul este ostil, iar caracterul are level mai mare va
+     face level up si va obtine cele 2 iteme, ce ii vor face movement-ul impaired. */
     return 0;
 }
